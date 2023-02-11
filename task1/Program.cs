0b106ca6e489fdfc54c5147978dd68f00104fc0c
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-11 23:02:44 +0300

    task1 is adding

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+  System.Console.WriteLine("Первое число большее");
+}
+else
+{
+  System.Console.WriteLine("Второе число большее");
+}
