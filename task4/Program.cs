9ccca705223ac548a7c4a4255559d3489e734a1b
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-11 23:04:45 +0300

    task4 is adding

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i = i + 1)
+  if ((i % 2) == 0)
+  {
+    System.Console.Write(i + " ");
+  }
