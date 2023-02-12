c51e4e59502f753d2ef70dd60850b7733c868ad6
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-12 19:00:09 +0300

    new task seminar2_task1

---
+++ seminar2_task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+  System.Console.WriteLine("Вторая цифра трехзначного числа равна: " + ((number / 10) - (number / 100)*10));
+}
+else
+{
+  System.Console.WriteLine($"Число {number} не трехзначное");
+}
