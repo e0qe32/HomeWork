d416ac48177129a19037d6257d8820b57f3212c8
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-12 19:43:47 +0300

    new task seminar2_task2

---
+++ seminar2_task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <= 99)
+{
+  System.Console.WriteLine("Третьей цифры нет");
+}
+else if (num >= 100 && num <= 999)
+{
+  System.Console.WriteLine("Третья цифра равна: " + num % 10);
+}
+else if (num >= 1000 && num <= 9999)
+{
+  System.Console.WriteLine("Третья цифра равна: " + ((num % 100 - num %10)/10));
+}
+else if (num >= 10000 && num <= 99999)
+{
+  System.Console.WriteLine("Третья цифра равна: " + ((num % 1000 - num %100)/100));
+}
