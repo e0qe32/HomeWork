93dbf39c31b9bda23fef3ac0a667003246246c2a
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-20 16:59:34 +0300

    seminar4_task2 is added

---
+++ seminar4_task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
+// цифр в числе. 452 -> 11; 82 -> 10; 012 -> 12
+
+int ReadInt(string text)
+{
+  System.Console.WriteLine(text);
+  return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int Summa(int number)
+{
+  int sum = 0;
+  while(number > 0)
+  {
+    sum = sum + (number % 10);
+    number = number / 10;
+  }
+  return sum;
+}
+
+
+int number = ReadInt("Введите число: ");
+System.Console.WriteLine(($"Сумма цифр = {Summa(number)}"));
+
+
+
+
