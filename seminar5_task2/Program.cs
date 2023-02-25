4ad902fd9c4cc371ec6b774f3e90a0a46a3a8b25
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-02-26 00:59:35 +0300

    seminar5_task2 is added

---
+++ seminar5_task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях. [3, 7, 23, 12] -> 19; 
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+  int[] array = new int[size];
+  Random rand = new Random();
+  for (int i = 0; i < size; i = i + 1)
+  {
+    array[i] = rand.Next(leftRange, rightRange + 1);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{  
+  System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int Summa(int[] array)
+{
+  int summa = 0;
+  for(int i = 0; i < array.Length; i = i + 1)
+  {
+    if(i % 2 != 0)
+    {
+      summa = summa + array[i];
+    }
+  }
+  return summa;
+}
+
+const int SIZE = 17;
+const int LEFT_RANGE = -10;
+const int RIGHT_RANGE = 10;  
+int[] array = GenerateArray(SIZE, LEFT_RANGE, RIGHT_RANGE); 
+PrintArray(array); 
+System.Console.WriteLine($"Сумма элементов массива на нечетных позициях: {Summa(array)}");
