ad847b9c911683b47c2819f1e4ba0d2da6d5030c
Egor Erokhov <egor.erokhov1976@mail.ru>
2023-03-12 09:18:23 +0300

    seminar9_task1 is added

---
+++ seminar9_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии. 
+// N = 5 -> "5, 4, 3, 2, 1";  N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int InputValue(string text)
+{
+  System.Console.WriteLine(text);
+  return Convert.ToInt32(Console.ReadLine());
+  
+}
+
+void AllNumbers(int n)
+{
+  if(n == 0)
+  {
+    return;
+  }
+  Console.Write(n + " ");
+  AllNumbers(n - 1);
+}
+
+AllNumbers(InputValue("Введите число: "));
+
